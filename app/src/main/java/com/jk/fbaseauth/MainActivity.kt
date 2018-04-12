c2069e6b253cd42d5e867a72b704172ab3d74ca6
jk2pr <jk12051985>
2018-04-12 11:28:37 +0800

    New Commit

--- app/src/main/java/com/jk/fbaseauth/MainActivity.kt
+++ app/src/main/java/com/jk/fbaseauth/MainActivity.kt
@@ -2,16 +2,36 @@ package com.jk.fbaseauth
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    private var mAuth=FirebaseAuth.getInstance()
+    private val RC_SIGN_IN = 123
+
+    private var mAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        var currentUser=mAuth.currentUser
+        var currentUser = mAuth.currentUser
+
+
+        val providers = Arrays.asList(
+                AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+                AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build(),
+                AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build())
+// Create and launch sign-in intent
+//        startActivityForResult(
+//                AuthUI.getInstance()
+//                        .createSignInIntentBuilder()
+//                        .setAvailableProviders(providers)
+//                        .build(),
+//                RC_SIGN_IN)
+
 
     }
 }
